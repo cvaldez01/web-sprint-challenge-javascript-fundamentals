0818944ab5865d02e967a9dc9ddaa6d9d29a59f0
cvaldezo1 <cvaldez01@github.com>
2020-09-14 01:29:19 -0400

    reduce method done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -53,15 +53,21 @@ Using filter, create a new array of objects called lowPopulationAnimals which co
 */
 const  lowPopulationAnimals=zooAnimals.filter(function(item){
   return item.population <= 5;
-});
+},0);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: 
+a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+
+let populationTotal = zooAnimals.reduce((total,currentp)=>{
+return total += currentp.population;
+},0);
 console.log(populationTotal);
 
 
