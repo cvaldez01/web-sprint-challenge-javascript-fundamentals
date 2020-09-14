09d24131c7452f8bda24fbe03dff5882e9be18a2
cvaldezo1 <cvaldez01@github.com>
2020-09-14 19:51:14 -0400

    prototype continue..work

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -34,6 +34,9 @@ console.log(volume);
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
+const area=  voleme.call(CuboidMaker)({length:3,width:5,height:5}){
+}
+
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
